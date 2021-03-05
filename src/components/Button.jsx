e8455620603171233b2312871862273c5843a2fc
pedrolgsoares <pedrodevmob@gmail.com>
2021-03-05 16:06:24 -0300

    Projeto finalizado

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,8 +9,9 @@ export default props => {
     classes += props.double ? 'double' : ''
     classes += props.triple ? 'triple' : ''
     return (
-        <button className={classes}
-            onClick={e => props.click && props.click(props.label)}>
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
             {props.label}
         </button>
     )
